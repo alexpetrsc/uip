f3b336b853705b079510a26652de03c1bfaabb8f
alexpetrsc <alexpetrsc@proton.me>
2026-07-17 22:54:33 +0300

    Add CATEGORIES dictionary for file types

---
+++ folders.py
@@ -0,0 +1,39 @@
+CATEGORIES = {
+
+    "Images": [
+
+        "jpg",
+
+        "png",
+
+        "gif"
+
+    ],
+
+    "Documents": [
+
+        "pdf",
+
+        "docx",
+
+        "txt"
+
+    ],
+
+    "Archives": [
+
+        "zip",
+
+        "rar"
+
+    ],
+
+    "Videos": [
+
+        "mp4",
+
+        "avi"
+
+    ]
+
+}
